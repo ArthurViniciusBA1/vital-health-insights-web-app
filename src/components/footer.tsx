221a91af4c4c284c6ed731590ce38a11004d3422
Arthur <arthurnaocopia@gmail.com>
2025-05-27 21:51:30 -0300

    fix: nome da imagem UNA

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     
     return (
         <footer className="max-w-4xl h-20 mx-auto flex items-center justify-between self-end gap-12">
-            <Image src={"/logoUNA.svg"} width={117} height={50} alt={"Logo UNA"}/>
+            <Image src={"/LogoUNA.svg"} width={117} height={50} alt={"Logo UNA"}/>
             <Image src={"/VitalHeathLogo.svg"}  width={86} height={50} alt={"Logo Vital Health Insights"}/>
         </footer>
     );
